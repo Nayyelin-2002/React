adac4a51b474c2146eb1dca80f31d0737481668a
Nay Ye Lin <6531503165@lamduan.mfu.ac.th>
2023-12-10 20:01:23 +0700

    react day1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Stus from "./components/One/one/Stusx";
+//d htl ko css br pr lr tl
+import Form from "./components/One/one/Form";
+import { useState } from "react";
 function App() {
+  let [stus, setStus] = useState([]);
+
+  let member = (mminfo) => {
+    setStus([...stus, mminfo]);
+  };
+
+  let showdates = <p>No Member yet</p>;
+
+  if (stus.length > 0) {
+    showdates = stus.map(function (stu) {
+      return <Stus name={stu.name} age={stu.age} key={stu.name} />;
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>{showdates}</section>
+      <Form addnewmember={member} />
     </div>
   );
 }
 
-export default App;
+export default App; //t chr ny yr ka pyn khw 3
