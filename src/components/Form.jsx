7609ddeacb98355e87a3cf23ba8eb2c720ee6e67
Nay Ye Lin <6531503165@lamduan.mfu.ac.th>
2023-12-21 23:17:50 +0630

    useeff and usereducer

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -57,13 +57,29 @@
 // }
 
 // export default Form;
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect, useReducer } from "react";
 import Card from "./Card";
 
 function Form(props) {
   //   let [name, setName] = useState("");
   //   let [address, setAddress] = useState("");
   //   let [email, setEmail] = useState("");
+  // let [username, setUsername] = useState("");
+  // let [count, setCount] = useState(0);
+  let firstReducer = (state, action) => {
+    switch (action.type) {
+      case "minus":
+        return { ...state, count: state.count - 1 }; // Update count using object spread syntax
+
+      case "plus":
+        return { ...state, count: state.count + 1 };
+      case "updateusername":
+        return { ...state, username: action.payload };
+      default:
+        throw new Error();
+    }
+  };
+  let [state, dispatch] = useReducer(firstReducer, { count: 0, username: "" });
 
   let reduser = useRef();
   let reflive = useRef();
@@ -83,6 +99,7 @@ function Form(props) {
       name: reduser.current.value,
       address: reflive.current.value,
       email: refmail.current.value,
+      username: state.username,
     };
     props.userleepl(info);
     //     setAddress("");
@@ -92,11 +109,13 @@ function Form(props) {
     console.log(
       reduser.current.value,
       reflive.current.value,
-      refmail.current.value
+      refmail.current.value,
+      state.username
     );
     reduser.current.value = "";
     reflive.current.value = "";
     refmail.current.value = "";
+    state.username = "";
   };
 
   //   let chgname = (event) => {
@@ -105,11 +124,25 @@ function Form(props) {
   //   let chgadd = (event) => {
   //     setAddress(event.target.value);
   //   };
-  //   let chgem = (event) => {
-  //     setEmail(event.target. value);
-  //   };
+
+  useEffect(() => {
+    console.log("I am useeff");
+  }, []);
   return (
     <Card key={refmail}>
+      {/* useeff 3 nee */}
+      <h1>Your username is - {state.username}</h1>
+      <div className="username">
+        <button onClick={() => dispatch({ type: "minus" })}>-</button>
+        <span>{state.count}</span>
+        <button
+          onClick={() => {
+            dispatch({ type: "plus" });
+          }}
+        >
+          +
+        </button>
+      </div>
       <form action="" onSubmit={submitform}>
         <div className="form-div">
           <label htmlFor="name">Name</label>
@@ -119,6 +152,17 @@ function Form(props) {
             //     value={reduser}
             //     onChange={chgname}
             ref={reduser}
+          />
+        </div>
+        <div className="form-div">
+          <label htmlFor="name">Userame</label>
+          <input
+            type="text"
+            id="username"
+            value={state.username}
+            onChange={(e) => {
+              dispatch({ type: "updateusername", payload: e.target.value });
+            }}
           />
         </div>
         <div className="form-div">
